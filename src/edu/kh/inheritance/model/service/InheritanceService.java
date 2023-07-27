6dc85716cbf7d788c56856b0b7de8fa2022034fe
unknown <vegayes@naver.com>
2023-07-27 14:05:43 +0900

    hello

---
+++ src/edu/kh/inheritance/model/service/InheritanceService.java
@@ -0,0 +1,200 @@
+package edu.kh.inheritance.model.service;
+
+import java.util.Scanner;
+
+import edu.kh.inheritance.model.vo.Employee;
+import edu.kh.inheritance.model.vo.Person;
+import edu.kh.inheritance.model.vo.Student;
+
+public class InheritanceService {
+
+	
+	// 상속 확인 예제
+	public void ex1() {
+		// Person을 상속받은 Student가 Person필드, 메소드를 
+		// 사용할 수 있는가?
+		
+		Person p = new Person();
+		
+//		p.name ="홍길동"; // private 때문에 직접접근불가( 에러발생 )
+		
+		p.setName("홍길동");
+		p.setAge(25);
+		p.setNationality("대한민국");
+		
+		System.out.println(p.getName());
+		System.out.println( p.getAge());
+		System.out.println(p.getNationality());
+		
+		System.out.println("-".repeat(20));
+		
+		// Student 객체 생성
+		Student std = new Student();
+		
+		// Student만의 고유한 필드 
+		std.setGrade(4);
+		std.setClassRoom(5);
+		
+		// Person 클래스로 부터 상속받은 필드
+		std.setName("고길동"); // Student에 없음. 상속받았으므로 setter 사용할 수 있음.
+		std.setAge(19);
+		std.setNationality("대한민국");
+		
+		// Student가 Person의 메소드 뿐 아니라 필드도 상속 받았는지 확인 
+		System.out.println(std.getName());
+		System.out.println(std.getAge());
+		System.out.println(std.getNationality());
+		System.out.println(std.getGrade());
+		System.out.println(std.getClassRoom());
+		
+		System.out.println("-".repeat(20));
+		
+		// Employee 객체 생성
+		Employee emp = new Employee();
+		
+		// Employee만의 고유한 필드
+		emp.setCompany("KH정보교육원");
+		
+		// Person 클래스로부터 상속받은 메소드
+		emp.setName("유은서");
+		emp.setAge(20);
+		emp.setNationality("대한민국");
+		
+		System.out.println(emp.getName());
+		System.out.println(emp.getAge());
+		System.out.println(emp.getNationality());
+		System.out.println(emp.getCompany());
+		
+		
+		//추가된 breath() 메서드 상속 확인하기 
+		p.breath();
+		std.breath();
+		emp.breath();
+		
+		// 상속의 특징 : 코드 추가 및 수정에 용이하다.
+		// => 부모에게 정의하면, 상속받은 자식들은 모두 부모의 것을 그대로 받아서 쓸 수 있음! 
+		
+		
+	}
+	
+	
+	// super() 생성자 사용방법
+	public void ex2() {
+		
+		// Student 매개변수 5개짜리 생성자 이용해서 함.
+		Student std = new Student("김철수", 17, "한국",1,3);
+		
+	System.out.println(std.getName());
+	System.out.println(std.getAge());
+	System.out.println(std.getNationality());
+	System.out.println(std.getGrade());
+	System.out.println(std.getClassRoom());
+		
+		
+		
+		
+	}
+
+	
+	// 오버라이딩 확인 예제
+	public void ex3() {
+		
+		Student std = new Student();
+		
+		Employee emp = new Employee();
+		
+		std.move(); // Person에 있는거 가져다가 씀.
+		
+		emp.move(); // 재정의한 (Employee)거 가져다가 씀.
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+	public void ex4() {
+		// 모든 클래스는 Object의 클래스의 후손임.
+		//  == 모든 클래스의 최상위 부모는 Object
+		
+		Person p = new Person(); // Object를 상속받은 Person 객체 생성
+		
+		Student std = new Student(); // Person을 상속받은 Student 객체 생성 
+		
+		// Object - Person - Student  
+		
+		System.out.println(p.hashCode());
+		System.out.println(std.hashCode());
+		// Person이 Object에서 물려받은 hashCode()를 Student가 또 물려받아 사용 
+		// ???????????????????????????????????????????????????????????????????????? 
+		
+		String str = "abc";
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(str.hashCode());
+		// Object-String 
+		System.out.println(sc.hashCode());
+		// Object-Scanner 
+		
+		
+		
+	}
+
+
+	public void ex5() {
+		//toString을 제일 오버라이딩에 많이 사용함.
+		
+		Person p = new Person("김철수",17,"한국");
+		
+		System.out.println(p.toString());
+		// Object(X) - Person 
+		
+		System.out.println(p); // toString을 바로 불러옴. 
+		// print 구문 수행 시, 참조 변수명을 작성하면, 
+		// 자동으로 toString()메소드를 호출해서 출력함.
+		
+		System.out.println("-".repeat(20));
+		
+		Student std = new Student("이백점",18 , " 미국", 2, 6);
+		
+		System.out.println(std.toString());
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
---
+++ src/edu/kh/inheritance/model/vo/Student.java
@@ -0,0 +1,135 @@
+package edu.kh.inheritance.model.vo;
+
+public class Student extends Person {
+				//  확장한다 부모 클래스를 
+	// => Student 클래스에 Person 클래스 내용을 연장한다.
+	// == Student 클래스에 Person 클래스 내용(필드, 메서드)을 추가하여 확장한다.
+	
+	//**** 상속 ****
+	//extends : 확장하다, 연장하다... 
+	
+	
+	/*
+	private String name;
+	private int age;
+	private String nationality;
+	*/
+	private int grade; 	// 학년
+	private int classRoom;  // 반
+	
+	
+	public Student() {
+		
+		/* Student() 객체 생성 시, 내부에 상속받은 Person객체를 생성하기 위한
+		 * Person 생성자 호출 코드 필요함!! 
+		 */
+		
+		//  super : 상위 
+		// super == Person (Student를 기준으로!) 
+		super(); // super() 생성자 
+		// 부모의 생성자를 호출하는 코드로 반드시, 자식생성자 '최상단'에 작성되어야 함! 
+		// ==> 부모의 생성자 실행(필드 초기화 + 특정기능 수행)  상위의 그분!! 이라고 부름 
+		
+		// * super() 생성자 때문에
+		// 자식 객체 내부에 부모 객체가 생성된다. 
+		
+		// * super() 생성자 미작성시
+		// 컴파일러가 컴파일 단계에서 자동으로 추가해줌. 
+		
+		
+		
+		
+		
+	}
+	
+	public Student(String name, int age, String nationality, int grade, int classRoom) {
+		/*
+		this.name = name;
+		this.age = age;
+		this.nationality = nationality;
+		*/
+		
+		// this.name = name;
+		// 왜? 부모의 필드에 private 접근 제한자가 있어서
+		// 아무리 물려받은 자식이라도 다른 객체이기 때문에 직접접근 불가
+
+		/* 1) 간접 접근 방법 사용 가능
+		setName(name);
+		setAge(age);
+		setNationality(nationality);
+		
+		// 부모의 setter를 이용할 수 있지만, 비효율적이다.
+		*/
+	
+		// 2) super를 이용해 부모의 매개변수 생성자 호출
+		super(name, age, nationality); // 부모의 생성자 중, 매개변수 생성자 호출 
+		
+		
+		this.grade = grade;
+		this.classRoom = classRoom;
+		
+	}
+
+	
+	
+	// 메소드
+	// getter, setter 
+	/*
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	 */
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getClassRoom() {
+		return classRoom;
+	}
+
+	public void setClassRoom(int classRoom) {
+		this.classRoom = classRoom;
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		return super.toString() + " / " + grade + " / " + classRoom;
+			// 부모꺼 가져옴            
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
